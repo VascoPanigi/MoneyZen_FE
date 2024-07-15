4e647b60a139bdd0e629b5ebbe4b753e15a59fae
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-07-15 18:11:32 +0200

    adds font style

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/App.scss";
 import LoginPage from "./components/LoginPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyNavbar from "./components/MyNavbar";
 
 function App() {
   return (
     <>
+      <MyNavbar />
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { fetchUserAction } from "../redux/actions";
@@ -22,24 +22,31 @@ const LoginPage = () => {
   };
 
   return (
-    <Form onSubmit={handleLoginSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
-        <Form.Text className="text-muted">We&apos;ll never share your email with anyone else.</Form.Text>
-      </Form.Group>
+    <Container fluid className="login-page-container">
+      <Row>
+        <Col className="login-container">
+          <Form onSubmit={handleLoginSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+              <Form.Text className="text-muted">We&apos;ll never share your email with anyone else.</Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 };
 
